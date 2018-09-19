dd945db34c4ffa95b710e18f8b6d2b37af769be3
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-09-19 10:15:12 -0400

    added three more team logos and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,17 @@ import EPL_logo from '../images/EPL_logo.svg'
 import Arsenal from '../images/ArsenalLogo.svg'
 import Bournemouth from '../images/BournemouthLogo.svg'
 import Brighton from '../images/BrightonLogo.svg'
+import Burnley from '../images/BurnleyLogo.svg'
+import CardiffCity from '../images/CardiffCityLogo.svg'
+import Chelsea from '../images/ChelseaLogo.svg'
+
 
 const HeaderWrapper = styled.div`
     margin: auto;
     // background-color: #75fa93;
     background-color: #32063A;
     display: flex;
+    justify-content: space-between;
     
     h1 {
         font-size: 1.5em;
@@ -25,6 +30,7 @@ const HeaderWrapper = styled.div`
     .team-logo {
         height: 2em;
         display: flex;
+        justify-content: space-around;
     }
 `
 
@@ -36,6 +42,9 @@ class Header extends Component {
                 <a href="https://www.arsenal.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Arsenal} className="team-logo" alt="logo" /></a>
                 <a href="https://www.afcb.co.uk/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Bournemouth} className="team-logo" alt="logo" /></a>
                 <a href="https://www.brightonandhovealbion.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Brighton} className="team-logo" alt="logo" /></a>
+                <a href="http://www.burnleyfootballclub.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Burnley} className="team-logo" alt="logo" /></a>
+                <a href="https://www.cardiffcityfc.co.uk/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={CardiffCity} className="team-logo" alt="logo" /></a>
+                <a href="https://www.chelseafc.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Chelsea} className="team-logo" alt="logo" /></a>
                 
             </HeaderWrapper>
         );
