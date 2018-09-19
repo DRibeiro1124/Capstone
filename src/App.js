3b2147602f753fe1401e394d0601a3768443a83f
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-09-19 10:08:39 -0400

    added first three teams logos and link to home page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          <Header />
+
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         <p className="App-intro">
           hello, world!
-          <Header />
         </p>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
+import EPL_logo from '../images/EPL_logo.svg'
+import Arsenal from '../images/ArsenalLogo.svg'
+import Bournemouth from '../images/BournemouthLogo.svg'
+import Brighton from '../images/BrightonLogo.svg'
 
 const HeaderWrapper = styled.div`
     margin: auto;
-    background-color: purple;
-
+    // background-color: #75fa93;
+    background-color: #32063A;
+    display: flex;
+    
     h1 {
         font-size: 1.5em;
         color: palevioletred;    
+    }
+    
+    .EPL-logo {
+        height: 7em;
+        display: flex;
+        background-color: #75fa93
+    }
+
+    .team-logo {
+        height: 2em;
+        display: flex;
     }
 `
 
@@ -15,7 +32,11 @@ class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
-                <h1>Hi, this is a header from HeaderJS</h1>
+                <img src={EPL_logo} className="EPL-logo" alt="logo" />
+                <a href="https://www.arsenal.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Arsenal} className="team-logo" alt="logo" /></a>
+                <a href="https://www.afcb.co.uk/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Bournemouth} className="team-logo" alt="logo" /></a>
+                <a href="https://www.brightonandhovealbion.com/?utm_source=premier-league-website&utm_campaign=website&utm_medium=link"><img src={Brighton} className="team-logo" alt="logo" /></a>
+                
             </HeaderWrapper>
         );
     }
